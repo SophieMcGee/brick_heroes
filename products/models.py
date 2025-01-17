6c419159b93171f088e955a0d70ec597bdb7dcdf
Sophie Mcgee <sophiebmcgee@outlook.com>
2025-01-17 09:52:59 +0000

    Add models, fixtures and loaded product data

--- products/models.py
+++ products/models.py
@@ -9,7 +9,17 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = CloudinaryField('image', null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
