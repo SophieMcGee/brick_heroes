919a4a22005d176984c7917ea3746865484d6d77
Sophie Mcgee <sophiebmcgee@outlook.com>
2025-01-17 14:21:56 +0000

    Update url to use all_products instead of collections

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('profile/', views.profile, name='user_profile'),
-    path('collections/', views.collections, name='collections'),
+    path('all-products/', views.all_products, name='all_products'),
     path('my-borrowings/', views.my_borrowings, name='my_borrowings'),
     path('my-reviews/', views.my_reviews, name='my_reviews'),
     path('my-notifications/', views.my_notifications, name='my_notifications'),
