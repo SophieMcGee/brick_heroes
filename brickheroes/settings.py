4d6d3af82df89a8b03c053b5594e2ae0dbac76ae
SophieMcGee <sophiebmcgee@outlook.com>
2025-02-06 13:23:13 +0000

    Update signup template for responsiveness

--- brickheroes/settings.py
+++ brickheroes/settings.py
@@ -15,6 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     exec(open('env.py').read(), globals())
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
