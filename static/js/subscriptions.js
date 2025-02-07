7bc55853b25e529d3c79375aa38f001ddff4fde2
SophieMcGee <sophiebmcgee@outlook.com>
2025-02-07 14:32:30 +0000

    Update js file to redirect user

--- static/js/subscriptions.js
+++ static/js/subscriptions.js
@@ -1,25 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var stripePublicKey = document.getElementById("id_stripe_public_key").textContent;
-    var stripe = Stripe(stripePublicKey);
-
     document.querySelectorAll(".subscribe-button").forEach(function (button) {
         button.addEventListener("click", function (event) {
             event.preventDefault();
             var planId = event.target.dataset.planId;
 
-            fetch(`/subscriptions/subscribe/${planId}/`, {
-                method: "GET",
-                headers: {
-                    "X-Requested-With": "XMLHttpRequest",
-                },
-            })
-            .then(response => response.json())
-            .then(data => {
-                return stripe.redirectToCheckout({ sessionId: data.sessionId });
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
+            // Redirect user to Django's subscribe URL (which then redirects to Stripe)
+            window.location.href = `/subscriptions/subscribe/${planId}/`;
         });
     });
 });
