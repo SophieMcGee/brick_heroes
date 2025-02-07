542362a8750c3d71b1be22eb6dc49babedb0da65
SophieMcGee <sophiebmcgee@outlook.com>
2025-02-07 22:47:13 +0000

    Update urls and borrow button in product app

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from cart import views as cart_views
 from .views import add_review, delete_review
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('review/<int:product_id>/', add_review, name='add_review'),
     path('delete_review/<int:review_id>/', delete_review, name='delete_review'),
+    path('add/<int:product_id>/', cart_views.add_to_cart, name='add_to_cart'),
 ]
