42807567db5dd1865bde3c93e719ee8c93dd068a
SophieMcGee <sophiebmcgee@outlook.com>
2025-02-10 13:40:00 +0000

    Add sitemap file and update robots file

--- brickheroes/urls.py
+++ brickheroes/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from allauth.account.views import EmailVerificationSentView
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,6 +33,7 @@ urlpatterns = [
         name="account_email_verification_sent",
     ),
     path('subscriptions/', include('subscriptions.urls')),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
